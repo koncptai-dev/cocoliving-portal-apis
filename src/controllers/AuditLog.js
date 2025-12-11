35d2b088576eed6efd99fd76b581cdc3fb18dabd
Muhammed Rishad <muhammed.rishad@koncpt.ai>
2025-12-11 14:35:31 +0530

    removed the log creating api and made it as a function

--- src/controllers/AuditLog.js
+++ src/controllers/AuditLog.js
@@ -1,27 +1,5 @@
 const AuditLog = require("../models/auditLog");
 
-exports.createAuditLog = async (req, res) => {
-  try {
-    const { userId, description, role, entity, entityId } = req.body;
-    const auditLog = await AuditLog.create({
-      userId,
-      description,
-      role,
-      entity,
-      entityId,
-    });
-    return res.status(201).json({
-      message: "Audit log created successfully",
-      auditLog,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Failed to create audit log",
-      error: error.message,
-    });
-  }
-};
-
 exports.getAuditLogs = async (req, res) => {
   try {
     const auditLogs = await AuditLog.findAll();
--- src/routes/AuditLog.js
+++ src/routes/AuditLog.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const AuditLogController = require("../controllers/AuditLog");
 
-router.post("/create", AuditLogController.createAuditLog);
 router.get("/get", AuditLogController.getAuditLogs);
 
 module.exports = router;
