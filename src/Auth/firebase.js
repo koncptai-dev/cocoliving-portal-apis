1742e817358c9ce1e807caabe34cbba6555cfca4
Deepanshu Choudhary <deepanshu7419@gmail.com>
2026-02-11 16:15:52 +0530

    enabled firebase.js

--- src/Auth/firebase.js
+++ src/Auth/firebase.js
@@ -1,12 +1,12 @@
-// const admin = require('firebase-admin');
-// const path = require('path');
+const admin = require('firebase-admin');
+const path = require('path');
 
-// const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
+const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
 
-// if (!admin.apps.length) {
-//     admin.initializeApp({
-//         credential: admin.credential.cert(require(path.resolve(serviceAccountPath))),
-//     });
-// }
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert(require(path.resolve(serviceAccountPath))),
+    });
+}
 
-// module.exports = admin;
+module.exports = admin;
