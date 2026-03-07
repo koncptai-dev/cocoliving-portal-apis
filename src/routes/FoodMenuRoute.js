85aab7ba3dcb429339a91430e03a082df1da5f1d
rishad09-go <muhammed.rishad@koncpt.ai>
2026-03-07 18:23:49 +0530

    added image upload in food menu

--- src/routes/FoodMenuRoute.js
+++ src/routes/FoodMenuRoute.js
@@ -3,13 +3,14 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
 const authorizeRole = require("../middleware/authorizeRole");
+const uploadFoodImages = require('../middleware/foodMenuUpload');
 
 //add food menu by admin
 // router.post('/add', authMiddleware, FoodMenuController.createFoodMenu);
 // router.put('/edit/:id', authMiddleware, FoodMenuController.editFoodMenu);
 
 // admin
-router.post('/upsert', authMiddleware, authorizeRole(1,3), FoodMenuController.upsertFoodMenu);
+router.post('/upsert', authMiddleware, authorizeRole(1,3), uploadFoodImages, FoodMenuController.upsertFoodMenu);
 
 // admin
 router.get('/list', authMiddleware, authorizeRole(1,3), FoodMenuController.getFoodMenus);
